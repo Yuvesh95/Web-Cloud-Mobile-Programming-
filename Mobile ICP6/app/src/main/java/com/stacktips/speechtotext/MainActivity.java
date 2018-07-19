08233d5a96fefecf96f31bc268af87521d7e1473
Yuvesh95 <35549738+Yuvesh95@users.noreply.github.com>
2018-07-19 16:58:51 -0500

    ICP

---
+++ Mobile ICP6/app/src/main/java/com/stacktips/speechtotext/MainActivity.java
@@ -0,0 +1,159 @@
+package com.stacktips.speechtotext;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech tts;
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private static final String PREFS = "prefs";
+    private static final String NAME = "name";
+    private static String name = " ";
+    int i = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(PREFS,0);
+        editor = preferences.edit();
+
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
+
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.UK);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language is not supported");
+                    }
+                    speak("Hello, What is your name");
+
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
+        }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    private void speak(String text){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+
+        }else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+
+                    ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String inSpeech = res.get(0);
+                    recognition(inSpeech);
+                }
+                break;
+            }
+        }
+    }
+    private void recognition(String text){
+        Log.e("Speech",""+text);
+
+        //creating an array which contains the words of the answer
+        String[] speech = text.split(" ");
+
+        //the last word is our name
+        name = speech[speech.length-1];
+
+
+        //we got the name, we can put it in local storage and save changes
+        if ( i == 1){
+            editor.putString(NAME, name).apply();
+            i++;
+
+        }
+
+        //make the app tell our name
+        speak("Your name is "+preferences.getString(NAME,name)+"ask your question");
+
+        if(text.contains("I'm not feeling good what should I do")){
+            Log.e("Speech",""+text);
+            Log.e("Speech","I can understand Please tell your symptoms in short.");
+
+
+            speak("I can understand Please tell your symptoms in short.");
+        }
+        if(text.contains("high temperature body aches")){
+            speak("your symptoms looks like fever do you need medical assistance.");
+        }
+        if(text.contains("what medicine should I take")){
+            speak("Please take these medicines Tylenol Advil Aspirin.");
+
+        }
+        if(text.contains("what time is it")){
+            SimpleDateFormat sdfDate = new SimpleDateFormat("HH:mm");//dd/MM/yyyy
+            Date now = new Date();
+            String[] strDate = sdfDate.format(now).split(":");
+            if(strDate[1].contains("00"))
+                strDate[1] = "o'clock";
+            speak("The time is " + sdfDate.format(now));
+
+        }
+        if(text.contains("thank you my medical assistant")|| text.contains("thank you")){
+            //speak("Thank you yuvesh"+preferences.getString(NAME,null)+" for using this application.");
+            speak("thank you "+preferences.getString(NAME,name)+" please take care of your health.");
+            //speak("Thank you yuvesh take care of your health.");
+        }
+
+    }
+}
